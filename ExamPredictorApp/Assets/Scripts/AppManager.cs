0a8f8b48e4925505ef271349163c083c52764fae
guunterr <gerardrglowacki@gmail.com>
2018-09-19 12:23:38 +0100

    GUI Improvement
    Dropdown populated, Scripts starting.

--- ExamPredictorApp/Assets/Scripts/AppManager.cs
+++ ExamPredictorApp/Assets/Scripts/AppManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AppManager : MonoBehaviour {
 
+    public static List<string> subjectList = new List<string> {"English", "Maths", "Science"};
+    public static List<string> parameterList = new List<string> { "Sleep", "Alone study", "Tuition" };
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,5 @@ public class AppManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
---
+++ ExamPredictorApp/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public GameObject listItemPrefab;
+    public Button myTestsButton;
+    public Button getPredictionButton;
+    public TMP_Dropdown subjectDropdown;
+
+    private void PopulateDropdownMenu()
+    {
+        subjectDropdown.AddOptions(AppManager.subjectList);
+    }
+
+    private void ClearDropdownMenu()
+    {
+        subjectDropdown.ClearOptions();
+    }
+    
+    private void PopulateParameterList()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        ClearDropdownMenu();
+        PopulateDropdownMenu();
+    }
+}
