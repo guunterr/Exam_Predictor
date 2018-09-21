d5c91851e75a10beb6e59c90fd27eba882705ab8
guunterr <gerardrglowacki@gmail.com>
2018-09-21 15:58:00 +0100

    Built Main Menu, started My Test menu
    Next tasks on github todo list.

--- ExamPredictorApp/Assets/Scripts/AppManager.cs
+++ ExamPredictorApp/Assets/Scripts/AppManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class AppManager : MonoBehaviour {
 
-    public static List<string> subjectList = new List<string> {"English", "Maths", "Science"};
-    public static List<string> parameterList = new List<string> { "Sleep", "Alone study", "Tuition" };
+    public static List<string> subjectList;
+    public static List<string> parameterList;
+    public static List<int> parameterValues = new List<int> { 8, 3, 1 };
 
 	// Use this for initialization
 	void Start () {
--- ExamPredictorApp/Assets/Scripts/MainMenuScript.cs
+++ ExamPredictorApp/Assets/Scripts/MainMenuScript.cs
@@ -10,9 +10,24 @@ public class MainMenuScript : MonoBehaviour {
     public Button myTestsButton;
     public Button getPredictionButton;
     public TMP_Dropdown subjectDropdown;
+    public GameObject listItemPanel;
+    private List<GameObject> itemPanelObjects = new List<GameObject>();
 
     private void PopulateDropdownMenu()
     {
+        if (AppManager.subjectList == null)
+        {
+            Debug.LogWarning("In AppManager, Subject List is uninitialised (AppManager.subjectList)");
+            subjectDropdown.AddOptions(new List<string> { "No Subjects" });
+            return;
+        }
+
+        if (AppManager.subjectList.Count == 0)
+        {
+            Debug.Log("Subject List in Appmanager is empty (AppManager.subjectList)");
+            subjectDropdown.AddOptions(new List<string> {"No Subjects"});
+            return;
+        }
         subjectDropdown.AddOptions(AppManager.subjectList);
     }
 
@@ -23,12 +38,48 @@ public class MainMenuScript : MonoBehaviour {
     
     private void PopulateParameterList()
     {
+        if (AppManager.parameterList == null)
+        {
+            Debug.LogWarning("In AppManager, parameterList list variable is not initialised");
+            return;
+        }
 
+        if (AppManager.parameterValues == null)
+        {
+            Debug.LogWarning("In AppManager, parameterValues list variable is not initialised");
+            return;
+        }
+
+        if (AppManager.parameterList.Count != AppManager.parameterValues.Count)
+        {
+            Debug.LogWarning("In Appmanager, the amount of parameters is not the same as the amount of values (parameterList != parameterValues).");
+            return;
+        }
+
+        for (int i = 0; i < AppManager.parameterList.Count; i++)
+        {
+            GameObject newListItem = Instantiate(listItemPrefab, listItemPanel.transform);
+            newListItem.GetComponentInChildren<TMP_Text>().SetText(AppManager.parameterList[i] + " " + AppManager.parameterValues[i].ToString());
+            itemPanelObjects.Add(newListItem);
+        }
+
+    }
+
+    private void ClearParameterList()
+    {
+        for (int i = 0; i < itemPanelObjects.Count; i++)
+        {
+            Destroy(itemPanelObjects[i]);
+        }
+        itemPanelObjects.Clear();
     }
 
     public void OnEnable()
     {
         ClearDropdownMenu();
         PopulateDropdownMenu();
+        ClearParameterList();
+        PopulateParameterList();
+        ClearParameterList();
     }
 }
