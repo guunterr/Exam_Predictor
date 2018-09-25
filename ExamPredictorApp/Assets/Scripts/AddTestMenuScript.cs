2765eb77096afb6455ce50ba4468632893bd42de
guunterr <gerardrglowacki@gmail.com>
2018-09-25 15:59:35 +0100

    Started AddTestMenu
    Started AddTestMenu, HOW DO YOU INPUT A DATE?

---
+++ ExamPredictorApp/Assets/Scripts/AddTestMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddTestMenuScript : MonoBehaviour {
+    public Button homeButton;
+    public TMP_Dropdown subjectDropdown;
+    public TMP_InputField sleepTimeField;
+    public TMP_InputField studyTimeField;
+    public TMP_InputField sleepRangeField;
+    public TMP_InputField tuitionTimeField;
+    public TMP_InputField testScore;
+    public Button finishButton;
+}
--- ExamPredictorApp/Assets/Scripts/Test.cs
+++ ExamPredictorApp/Assets/Scripts/Test.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Test")]
 public class Test: ScriptableObject {
     //TODO :remove scriptable object inheritance for release, chenga public to public readonly and create a contructor
+    public int id;
     public float score;
     public float hoursSleep;
     public float hoursStudy;
